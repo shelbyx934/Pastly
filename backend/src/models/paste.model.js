ee6de951f46e9a19c8443fa94035334defd984d4
Mohit Raj <mohitrajk535@gmail.com>
2026-06-01 14:29:16 +0530

    Created Paste Schema for Mongo DB

--- backend/src/models/paste.model.js
+++ backend/src/models/paste.model.js
@@ -1,1 +1,32 @@
 import mongoose from 'mongoose';
+
+const pasteSchema = new mongoose.Schema({
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    fileid: {
+        type: Number,
+        required: true
+    },
+    contentSize: {
+        type: Number,
+        required: true
+    },
+    expiresAt: {
+        type: Date,
+        required: true
+    },
+    lastAccessedAt: {
+        type: Date,
+        required: true,
+    }
+},);
+
+pasteSchema.index({
+    expiresAt: 1
+});
+
+export const Paste = mongoose.model('Paste', pasteSchema);
